0ff9800c945004dd4a86b9274d491a89792c6351
Marko Pavičić <mpavicic99@gmail.com>
2021-09-15 16:33:28 +0200

    Update layouts

--- app/src/main/java/com/markopavicic/factorynewsreader/NewsViewHolder.java
+++ app/src/main/java/com/markopavicic/factorynewsreader/NewsViewHolder.java
@@ -1,7 +1,10 @@
 package com.markopavicic.factorynewsreader;
 
+import android.text.method.ScrollingMovementMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,12 +16,14 @@ public class NewsViewHolder extends RecyclerView.ViewHolder implements View.OnCl
     private final NewsClickListener clickListener;
     private final ImageView ivNews;
     private final TextView tvNewsHeadline;
+    private final ScrollView scrollView;
 
     public NewsViewHolder(@NonNull View itemView, NewsClickListener clickListener) {
         super(itemView);
         this.clickListener = clickListener;
         ivNews = itemView.findViewById(R.id.ivNews);
         tvNewsHeadline = itemView.findViewById(R.id.tvNewsHeadlineRecycler);
+        scrollView = itemView.findViewById(R.id.tvScroll);
         itemView.setOnClickListener(this);
     }
 
@@ -30,5 +35,13 @@ public class NewsViewHolder extends RecyclerView.ViewHolder implements View.OnCl
     {
         Picasso.with(itemView.getContext()).load(imageUri).into(ivNews);
         tvNewsHeadline.setText(headline);
+        tvNewsHeadline.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                view.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+        tvNewsHeadline.setMovementMethod(new ScrollingMovementMethod());
     }
 }
